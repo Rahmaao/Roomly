ed3cc0c095bac12ed3dd1ed1eacb8a2ed852e66d
Femi Ayo-Tubosun <femiayotubosun@gmail.com>
2021-04-12 21:26:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,6 @@
 from flask import Flask
-from flask_mysqldb import MySQL
-import MySQLdb.cursors
-import re
 
 app = Flask(__name__)
-app.config.from_object('config.Config')
-
-# Intialize MySQL
-mysql = MySQL(app)
+app.config.from_object('config.Config'
 
 import views
-
-# http://localhost:5000/main - this will be the logout page
-
-
